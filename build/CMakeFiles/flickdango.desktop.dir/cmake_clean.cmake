f8e0e4ea7cd5d5fabcab20fa258120ab655776b0
Martin Ferretti <ferrettimartin@gmail.com>
2018-11-17 00:57:56 -0500

    Minor update

---
+++ build/CMakeFiles/flickdango.desktop.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flickdango.desktop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/flickdango.desktop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
